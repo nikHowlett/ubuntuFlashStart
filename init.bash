d8c8768cde67d2aab06c5dc843f5fb38ad544361
Nik <nikhowlett@gmail.com>
2020-02-29 15:06:27 +0000

    Create init.bash

---
+++ init.bash
@@ -0,0 +1,4 @@
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo apt install ./google-chrome-stable_current_amd64.deb
+sudo wget https://builds.parsecgaming.com/package/parsec-linux.deb
+sudo apt install ./parsec-linux.deb
