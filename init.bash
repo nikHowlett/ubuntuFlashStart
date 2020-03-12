0cd2c179aa6304aac25e3e63081f715e5b6f884e
Nik <nikhowlett@gmail.com>
2020-03-12 14:53:49 +0000

    Add openVPN_1

--- init.bash
+++ init.bash
@@ -2,3 +2,4 @@ wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo apt install ./google-chrome-stable_current_amd64.deb
 sudo wget https://builds.parsecgaming.com/package/parsec-linux.deb
 sudo apt install ./parsec-linux.deb
+sudo apt-get install openvpn -y
