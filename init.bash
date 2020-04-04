f6b3baa4d689f07257df4c8f4c5e48e172f6924a
Nik <nikhowlett@gmail.com>
2020-04-03 20:55:33 -0400

    Add Runescape XD

--- init.bash
+++ init.bash
@@ -3,3 +3,6 @@ sudo apt install ./google-chrome-stable_current_amd64.deb
 sudo wget https://builds.parsecgaming.com/package/parsec-linux.deb
 sudo apt install ./parsec-linux.deb
 sudo apt-get install openvpn -y
+sudo apt-get install gnome-screenshot
+sudo wget https://github.com/runelite/launcher/releases/download/2.1.0/RuneLite.AppImage
+sudo chmod 777 RuneLite.AppImage
