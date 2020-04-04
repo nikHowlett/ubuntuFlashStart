ff88396a93af37996f884065cea46416c8b6f072
Nik <nikhowlett@gmail.com>
2020-04-04 00:12:40 -0500

    auto timezone change
    also automatically install chrome without asking

--- init.bash
+++ init.bash
@@ -1,5 +1,6 @@
+sudo timedatectl set-timezone EST
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-sudo apt install ./google-chrome-stable_current_amd64.deb
+sudo apt install ./google-chrome-stable_current_amd64.deb -y
 sudo wget https://builds.parsecgaming.com/package/parsec-linux.deb
 sudo apt install ./parsec-linux.deb
 sudo apt-get install openvpn -y
