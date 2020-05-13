c5b46a6938c8829a9689433d8954c5619b2b0f67
Nik <nikhowlett@gmail.com>
2020-05-12 20:48:28 -0400

    auto install pip

--- init.bash
+++ init.bash
@@ -4,9 +4,10 @@ sudo apt install ./google-chrome-stable_current_amd64.deb -y
 sudo wget https://builds.parsecgaming.com/package/parsec-linux.deb
 sudo apt install ./parsec-linux.deb
 sudo wget install https://download.anydesk.com/linux/anydesk_5.5.5-1_amd64.deb
-sudo apt install ./anydesk_5.5.5-1_amd64.deb
+sudo apt install ./anydesk_5.5.5-1_amd64.deb -y
 sudo apt-get install openvpn -y
 sudo apt-get install gnome-screenshot
 sudo wget https://github.com/runelite/launcher/releases/download/2.1.0/RuneLite.AppImage
 sudo chmod 777 RuneLite.AppImage
 sudo apt-get install xdotool -y
+sudo apt-get install python3-pip -y
